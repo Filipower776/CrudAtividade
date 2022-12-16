218796b3586ac44e81d6ae8ac3d3f3fe44ef41ee
Filipe Coura <filipefcoura888@gmail.com>
2022-12-15 21:25:22 -0300

    tahsggaag

---
+++ CRUD atividade/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace CRUD_atividade.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
